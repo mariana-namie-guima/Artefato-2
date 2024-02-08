48944380a9869b3d4b0e1ac703bc66a2e19b5f18
mariana-namie-guima <159195122+mariana-namie-guima@users.noreply.github.com>
2024-02-07 22:15:52 -0300

    codigo correto

--- teste.js
+++ teste.js
@@ -1,38 +1,53 @@
 var config = {
     type: Phaser.AUTO,
-    width: 600,
-    height: 400, 
-    }
+    width: 800,
+    height: 600,
+    physics: {
+        default: "arcade",
+        arcade: {
+            gravity:{y:0},
+            debug: false,
+        }
+    },
     scene: {
-        preload: preload;
-        create: create;
-        update: update;
+        preload: preload,
+        create: create,
+        update: update,
     }
+};
 
 var game  = new Phaser.Game(config);
 
 var player;
 
 function preload(){
-    this.load.image('inicioBackgroung', 'background/testeBackgroung.png')
-    this.load.image('player', 'personagem/gato.png')
+    this.load.image('bg', 'assets/testeBg.png')
+    this.load.image('player', 'assets/gato.png')
 
 }
 
 function create(){
-    this.add.image(0, 0, 'inicioBackground').setOrigin(0,0);
-    this.add.image(300, 200, 'player').setScale(0.1);
+    this.add.image(0, 0, 'bg').setOrigin(0,0);
+    this.add.image(300, 200, 'player').setScale(0.01);
     this.player = this.physics.add.sprite(400, 150, "player")
-    this.cursors = this.input.keyboard.creatCursorKeys();
+    this.cursors = this.input.keyboard.createCursorKeys();
 }
 
 
 function update() {
+    this.player.setVelocityX(0);
+    this.player.setVelocityY(0);
     if(this.cursors.left.isDown){
         console.log("esquerda");
-        this.player.stVelocityX(-100);
+        this.player.setVelocityX(-100);
     }else if (this.cursors.right.isDown){
         console.log("direita");
-        this.player.stVelocityX(100);
+        this.player.setVelocityX(100);
+    }else if (this.cursors.up.isDown){
+        console.log("cima");
+        this.player.setVelocityY(-100);
+    }else if (this.cursors.down.isDown){
+        console.log("baixo");
+        this.player.setVelocityY(100);
     }
 }
